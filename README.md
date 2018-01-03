e32b11c6300ee208210c0bfebd70aeeb864bca3d
Charlie Harrington <charlie@floydhub.com>
2018-01-03 11:30:55 -0800

    cozmo now taking photos for 5 seconds

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ source ~/.env/cozmo/bin/activate
 pip install -r requirements.txt
 ```
 
+
+## Ask Cozmo to help generate a dataset
+
+Let's ask Cozmo to take pictures of something, let's say a rock
+```bash
+python3 cozmo-paparazzi.py rock
+```
--- cozmo-paparazzi.py
+++ cozmo-paparazzi.py
@@ -1,9 +1,38 @@
 import cozmo
 from cozmo.util import degrees
+import time
+import sys
+import os
+
+# GLOBALS
+imageNumber = 0
+directory = '.'
+
+
+def on_new_camera_image(evt, **kwargs):
+	pilImage = kwargs['image'].raw_image
+	global directory
+	pilImage.save(f"data/{directory}/{directory}-{kwargs['image'].image_number}.jpeg", "JPEG")
 
 def cozmo_program(robot: cozmo.robot.Robot):
+	
+	# Make sure Cozmo's head and arm are at reasonable levels
 	robot.set_head_angle(degrees(10.0)).wait_for_completed()
 	robot.set_lift_height(0.0).wait_for_completed()
+
+	# Set directory to the Category that Cozmo is going to photograph
+	global directory
+	directory = sys.argv[1]
+	if not os.path.exists('data'):
+		os.makedirs('data')
+	if not os.path.exists(f'data/{directory}'):
+		os.makedirs(f'data/{directory}')
+
+	# Anytime Cozmo sees a "new" image, take a photo
+	robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
+
+	# Do this for 5 seconds, and then quit with a Lady Gaga quote
+	time.sleep(5)
 	print("I'm your biggest fan. I'll follow you until you love me")
 
 cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
