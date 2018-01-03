c6ed1f8f74ad48930743035ce36dabe2844d6399
Charlie Harrington <charlie@floydhub.com>
2018-01-03 14:10:06 -0800

    update script and add the retrain script from google code lab

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ pip install -r requirements.txt
 
 ## Ask Cozmo to help generate a photo dataset
 
-Let's ask Cozmo to take pictures of something, let's say a bottle of seltzer. Place Cozmo directly in front of a bottle of seltzer on the ground, and make sure that he has enough space to rotate around the seltzer can to take pictures.
+Let's ask Cozmo to take pictures of something, let's say a bottle of seltzer. Place Cozmo directly in front of a bottle of seltzer on the ground, and make sure that he has enough space to rotate around the seltzer can to take pictures. Be sure to enter the name of the object that Cozmo is photographing when you run the script.
 ```bash
-python3 cozmo-paparazzi.py rock
+python3 cozmo-paparazzi.py seltzer
 ```
 
 ## References
 
-This project is an extension of @nheidloff's [Cozmo visual recognition project](https://github.com/nheidloff/visual-recognition-for-cozmo-with-tensorflow).
+This project is an extension of @nheidloff's [Cozmo visual recognition project](https://github.com/nheidloff/visual-recognition-for-cozmo-with-tensorflow) and the [Google Code Labs TensorFlow for Poets project](https://codelabs.developers.google.com/codelabs/tensorflow-for-poets/#0).
--- cozmo-paparazzi.py
+++ cozmo-paparazzi.py
@@ -19,18 +19,24 @@ def on_new_camera_image(evt, **kwargs):
 def move_to_next_side(robot: cozmo.robot.Robot):
 	robot.turn_in_place(degrees(-45)).wait_for_completed()
 	robot.drive_straight(distance_mm(400), speed_mmps(200), False, False, 0).wait_for_completed()
-	robot.turn_in_place(degrees(135)).wait_for_completed()
+	robot.turn_in_place(degrees(130)).wait_for_completed()
+	take_photos(robot)
+
+def take_photos(robot: cozmo.robot.Robot):
+	global liveCamera
 	# Start photo sesh
 	liveCamera = True
-	robot.drive_straight(distance_mm(200), speed_mmps(100), False, False, 0).wait_for_completed()
-	robot.drive_straight(distance_mm(-200), speed_mmps(100), False, False, 0).wait_for_completed()
+	time.sleep(1)
+	robot.drive_straight(distance_mm(100), speed_mmps(100), False, False, 0).wait_for_completed()
+	robot.drive_straight(distance_mm(-100), speed_mmps(100), False, False, 0).wait_for_completed()
 	# Stop photo sesh
 	liveCamera = False
+	time.sleep(1)
 
 def cozmo_program(robot: cozmo.robot.Robot):
 	
 	# Make sure Cozmo's head and arm are at reasonable levels
-	robot.set_head_angle(degrees(2.5)).wait_for_completed()
+	robot.set_head_angle(degrees(10.0)).wait_for_completed()
 	robot.set_lift_height(0.0).wait_for_completed()
 
 	robot.say_text(f"I'm going to take photos of {sys.argv[1]}").wait_for_completed()
@@ -44,20 +50,19 @@ def cozmo_program(robot: cozmo.robot.Robot):
 		os.makedirs(f'data/{directory}')
 
 	# Anytime Cozmo sees a "new" image, take a photo
-	global liveCamera
 	robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
 
 	# Initial photo sesh
-	liveCamera = True
 	robot.drive_straight(distance_mm(-200), speed_mmps(100), False, False, 0).wait_for_completed()
-	liveCamera = False
+	take_photos(robot)
 
 	# Get all the angles
 	for i in range(3):
 		move_to_next_side(robot)
 
-	# Wait a second, and then quit with a Lady Gaga quote
-	time.sleep(1)
-	robot.say_text("I'm your biggest fan. I'll follow you until you love me").wait_for_completed()
+	# And we're done here
+	robot.say_text("All done!").wait_for_completed()
+	robot.play_anim_trigger(cozmo.anim.Triggers.MajorWin).wait_for_completed()
+	
 
 cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
